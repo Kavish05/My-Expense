6a9626a44006121a1b6cb7ed2c8cd7c5f19e8a1f
Kavish05 <96923317+Kavish05@users.noreply.github.com>
2021-12-31 23:05:03 +0400

    5th

--- webpages/dbconnect.php
+++ webpages/dbconnect.php
@@ -5,10 +5,10 @@
 	$db = "expense";
 
 	// Create connection
-	//$conn = mysqli_connect($servername, $username, $password, $db);
+	$conn = mysqli_connect($servername, $username, $password, $db);
 
 	// Check connection
-	if (!$conn = mysqli_connect($servername, $username, $password, $db)) {
+	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
 	//echo "Connected successfully !";
