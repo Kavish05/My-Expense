91373bf2c9b7ff933e91b82661cd54a0e816297f
Kavish05 <96923317+Kavish05@users.noreply.github.com>
2021-12-31 22:58:24 +0400

    3rd

--- webpages/dbconnect.php
+++ webpages/dbconnect.php
@@ -1,19 +1,15 @@
-<html>
-	<body>
-		<?php
-			$servername = "fdb31.runhosting.com";
-			$username = "4016712_expense"; //username of user
-			$password = "Expense01"; //password of above user
-			$db = "expense";
+<?php
+	$servername = "fdb31.runhosting.com";
+	$username = "4016712_expense"; //username of user
+	$password = "Expense01"; //password of above user
+	$db = "expense";
 
-			// Create connection
-			$conn = mysqli_connect($servername, $username, $password, $db);
+	// Create connection
+	//$conn = mysqli_connect($servername, $username, $password, $db);
 
-			// Check connection
-			if (!$conn) {
-				die("Connection failed: " . mysqli_connect_error());
-			}
-			//echo "Connected successfully !";
-		?>
-	</body>
-</html>
+	// Check connection
+	if (!$conn = mysqli_connect($servername, $username, $password, $db)) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	//echo "Connected successfully !";
+?>
