c25362160a2f3b406087539432fac49475fcda93
Kavish05 <96923317+Kavish05@users.noreply.github.com>
2022-01-02 13:18:08 +0400

    1

--- getData.php
+++ getData.php
@@ -1,23 +1,28 @@
 <?php
-$servername = "fdb31.runhosting.com";
-$username = "4016712_expense"; //username of user
-$password = "expense02"; //password of above user
-$db = "4016712_expense";
-if (!$conn = mysqli_connect($servername, $username, $password, $db)){
-     # code...
-    echo "Problem in database connection! Contact administrator!" . mysqli_error();
- }else{
-         $sql ="SELECT category,sum(amount) FROM transaction WHERE username= '$name' GROUP BY category";
-         $result = mysqli_query($conn,$sql);
-         $chart_data="";
-         while ($row = mysqli_fetch_array($result)) { 
+   $servername = "fdb31.runhosting.com";
+   $username = "4016712_expense"; //username of user
+   $password = "expense02"; //password of above user
+   $db = "4016712_expense";
+   // Create connection
+   $conn = mysqli_connect($servername, $username, $password, $db);
+
+   // Check connection
+   if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+   }
+   //echo "Connected successfully !";
+   }else{
+   $sql ="SELECT category,sum(amount) FROM transaction WHERE username= '$name' GROUP BY category";
+   $result = mysqli_query($conn,$sql);
+   $chart_data="";
+   while ($row = mysqli_fetch_assoc($result)) { 
+
+   $category[]  = $row['category'];
+   $amount[] = $row['amount'];
+   }
  
-            $Category[]  = $row['category'];
-            $Amount[] = $row['amount'];
-        }
  
- 
- }
+   }
  
  
 ?>
